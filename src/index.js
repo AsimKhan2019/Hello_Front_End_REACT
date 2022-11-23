899d89d1d90ec7158e030b7d7008bfcdcb82f6e1
AsimKhan2019 <asim.mahmood.khan@hotmail.com>
2022-11-23 21:22:49 +0500

    Updated linters and fixed errors

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './components/App';
-//import loadGreeting from './redux/actions';
+// import loadGreeting from './redux/actions';
 import store from './redux/configureStore';
-import { Provider } from 'react-redux'
 
-//store.dispatch(loadGreeting());
+// store.dispatch(loadGreeting());
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <App/>
+    <App />
   </Provider>,
-  //document.getElementById('root'),
+  // document.getElementById('root'),
 );
